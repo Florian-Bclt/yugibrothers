a9ed007122b8e39bbfe17ce67fe9d18e495661a7
Florian <bouclet.florian@hotmail.com>
2023-03-23 23:36:45 +0100

    hero section + navbar

---
+++ src/components/Hero.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+import styled from 'styled-components'
+import Navbar from './Navbar'
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  width: 1400px;
+  scroll-snap-align: center;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 2;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 20px;
+`;
+
+const Title = styled.h1`
+  font-size: 74px;
+`;
+
+const WhatWeDo = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+const Line = styled.img`
+  height: 5px;
+`;
+
+const Subtitle = styled.h2`
+  color: #da4ea2;
+`;
+
+const Desc = styled.p`
+  font-size: 24px;
+  color: lightgray;
+`;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 100px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
+
+const Right = styled.div`
+  flex: 3;
+  position: relative;
+`;
+
+const Img = styled.img`
+  width: 800px;
+  height: 600px;
+  object-fit: contain;
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  margin: auto;
+  animation: animate 2s infinite ease alternate;
+
+  @keyframes animate {
+    to{
+      transform: translateY(20px);
+    }
+  }
+`;
+
+function Hero() {
+  return (
+    <Section>
+      <Navbar />
+      <Container>
+        <Left>
+          <Title>Think. Make. Solve.</Title>
+          <WhatWeDo>
+            <Line src='./assets/line.png'/>
+            <Subtitle>What we do</Subtitle>
+          </WhatWeDo>
+          <Desc>Nous sommes heureux de vous présenter notre association autour du jeu Yu-Gi-Oh! Trading card.</Desc>
+          <Button>En savoir plus</Button>
+        </Left>
+        <Right>
+          {/* 3d model */}
+          <Img src='./assets/moon.png'/>
+        </Right>
+      </Container>
+    </Section>
+  )
+}
+
+export default Hero
